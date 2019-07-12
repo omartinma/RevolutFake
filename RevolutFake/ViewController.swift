9078fdddb0d7f17ba9adc765f2ddc3016391827d
Oscar Martin <martinm.oscar@gmail.com>
2019-07-12 14:29:11 +0200

    Adding different sizes for label of total amount

--- RevolutFake/ViewController.swift
+++ RevolutFake/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalAmountLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+         loadTotalAmount()
+       
     }
 
+    func loadTotalAmount(){
+        let currency = "€"
+        let mainAmount = "600"
+        let decimalSeparator = "."
+        let decimalAmount = "00"
+        let amountText = NSMutableAttributedString.init(string: currency+mainAmount+decimalSeparator+decimalAmount)
+        
+// Set currency
+        amountText.setAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 40),
+                                  NSAttributedString.Key.foregroundColor: UIColor.black],
+                                 range: NSMakeRange(0, 1))
+// Set main number
+        
+        amountText.setAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 38),
+                                  NSAttributedString.Key.foregroundColor: UIColor.black],
+                                 range: NSMakeRange(1, mainAmount.count))
+        // Set decimal separator
+        
+        amountText.setAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 30),
+                                  NSAttributedString.Key.foregroundColor: UIColor.black],
+                                 range: NSMakeRange(currency.count+mainAmount.count, 1))
+        
+        
+        // Set decimal number
+        amountText.setAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20),
+                                  NSAttributedString.Key.foregroundColor: UIColor.black],
+                                 range: NSMakeRange(currency.count+mainAmount.count + decimalSeparator.count, decimalAmount.count))
+        // set the attributed string to the UILabel object
+        totalAmountLabel.attributedText = amountText
+    }
 
 }
 
